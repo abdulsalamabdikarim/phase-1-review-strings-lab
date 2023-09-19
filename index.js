8a6a0cc8a2f5ec2b2cd283adfb97aa78764eee79
Abdulsalam Abdikarim <achalam00@gmail.com>
2023-09-19 19:50:16 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser = "Grace Hopper";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+//alt: const welcomeMessage = `Welcome to Flatbook, ` + currentUser + `!`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+//alt: const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
